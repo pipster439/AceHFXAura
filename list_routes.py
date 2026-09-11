0332b1caa45f21bf32a182ccc8c05822f7a470e0
pipster439 <fangbian29@gmail.com>
2026-09-12 07:00:40 +0800

    refactor: restructure project directory layout by adding modular source files and removing legacy test scripts

--- list_routes.py
+++
@@ -1,7 +0,0 @@
-with open(r'C:\Program Files (x86)\ASUS\ArmouryDevice\modules\keyboard\index.js', 'r', encoding='utf-8', errors='ignore') as f:
-    text = f.read()
-
-import re
-print("Matches:")
-for m in re.findall(r'case"([^"]+)":', text):
-    print(" ", m)
